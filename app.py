5df233eef745e5b65c1001481c1a55e8c1a16495
edutekno <54890481+edutekno@users.noreply.github.com>
2025-02-26 17:10:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import openai
 
-OPENAI_API_KEY=st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY=os.environ["OPENAI_API_KEY"]
 BASE_URL = "https://api.openai.com/v1"  # Default base URL OpenAI
 MODEL = "gpt-3.5-turbo"  # Model default
 MAX_TOKENS = 150  # Batas panjang jawaban
